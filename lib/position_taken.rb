1200e7d148ff131521c0a2f4909e061d38561c2b
Drew Hadley <drewhadley.guitar@gmail.com>
2020-11-06 21:42:31 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
     i = index
+<<<<<<< HEAD
     if board[i] == "" || board[i] == " " || board[i] == nil
         false
     elsif board[i] == "X" || board[i] == "O" 
@@ -9,3 +10,12 @@ def position_taken?(board, index)
          nil
     end
 end
+=======
+    if board[i] == "" || " " || nil
+        false
+    elsif board[i] == "X" || "O"
+        true
+    else nil
+    end
+end
+>>>>>>> ca8232eeb75ed84cd879dea33f34f23fedf3f233
